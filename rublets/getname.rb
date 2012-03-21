0e36ab296a008a73f7a46b530b10c978e30c0de4
craigmartin <craig@thegleemachine.com>
2012-03-21 16:25:37 -0700

    add getname

---
+++ rublets/getname.rb
@@ -0,0 +1,13 @@
+# simple ruby exercises
+puts 'Please enter your first name.'
+firstName = gets.chomp
+
+puts 'Now enter your middle name'
+middleName = gets.chomp
+
+puts 'Now enter your surname'
+surname = gets.chomp
+
+puts 'Hello ' + firstName + ' ' + middleName + ' ' + surname
+
+puts 'Thanks for your cooperation'
