9391f98ba875285bf78198cab9e6565941cc5ad8
craigmartin <craig@thegleemachine.com>
2012-03-21 16:21:59 -0700

    add ruby dir

---
+++ rublets/getfav.rb
@@ -0,0 +1,6 @@
+# ruby exercise
+puts 'Hi There! what\'s your favorite number?'
+
+favnum = gets.chomp
+
+puts "Number #{ (favnum.to_i + 1) } is bigger. Maybe you should consider this as your favourite number."
